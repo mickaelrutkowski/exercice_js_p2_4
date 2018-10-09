a53c059b83986684485ab11ae6e3d912139dbb77
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-09 22:33:50 +0200

    exercice p2 4

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function test(){
+  document.getElementById('lastname').value= "";
+    document.getElementById('firstname').value= "";
+      document.getElementById('city').value= "";
+//les "" donne la valeur null
+}
